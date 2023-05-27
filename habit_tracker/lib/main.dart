420e895ef2364bd0cb6e8e7989262cdd87ecfd66
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-05-27 19:34:18 +0530

    Implement Basic App

--- habit_tracker/lib/main.dart
+++ habit_tracker/lib/main.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:habit_tracker/pages/home_page.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  // initialize hive
+  await Hive.initFlutter();
+
+  // open a box
+  await Hive.openBox("Habit_Database");
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:  MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.green),
     );
   }
 }
